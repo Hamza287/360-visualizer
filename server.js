126747e3cacae183a4dedea6ed2948ffc9639a59
hamza ibrahim <hamzaibrahimflutter@gmail.com>
2024-07-20 11:30:17 +0500

    cors added

--- server.js
+++ server.js
@@ -6,15 +6,7 @@ const app = express();
 const port = 3000;
 const allowedOrigins = ['http://3.112.221.212:3000', 'http://localhost:3000'];
 app.use(cors({
-    origin: function (origin, callback) {
-        // Allow requests with no origin (like mobile apps or curl requests)
-        if (!origin) return callback(null, true);
-        if (allowedOrigins.indexOf(origin) === -1) {
-            const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
-            return callback(new Error(msg), false);
-        }
-        return callback(null, true);
-    }
+    origin: 'http://3.112.221.212:3000'
 }));
 app.use(express.static('public'));
 
